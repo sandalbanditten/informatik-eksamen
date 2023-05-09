2157b238d0268119fa40738c31acd1e16244277f
Mathias Bøttger <mathiasbottger@gmail.com>
2023-05-09 12:42:32 +0200

    idfk

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod workout;
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(360.0, 800.0)),
+        initial_window_size: Some(egui::vec2(375.0, 812.0)),
         ..Default::default()
     };
 
@@ -66,18 +66,33 @@ impl Default for App {
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("rST");
-            ui.separator();
 
-            // Workouts
-            ui.heading("Workouts:");
-            // Iterator is reversed to show more recent workouts
-            // (with bigger timestamps) before earlier workouts
-            for (i, workout) in self.workouts.iter().rev().enumerate() {
-                ui.label(format!("Workout {}:", i + 1));
-                ui.label(format!("{}", workout));
-            }
+        let mut style = (*ctx.style()).clone();
+        
+        for (_text_style, font_id) in style.text_styles.iter_mut() {
+            font_id.size = 20.0 // whatever size you want here
+
+        }
+
+        ctx.set_style(style);
+        
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.heading("rST");
+                ui.separator();
+
+                // Workouts
+                ui.heading("Workouts:");
+                // Iterator is reversed to show more recent workouts
+                // (with bigger timestamps) before earlier workouts
+                for (i, workout) in self.workouts.iter().rev().enumerate() {
+                    ui.label(format!("Workout {}:", i + 1));
+                    ui.label(format!("{workout}"));
+                }
+            });
+            
+
+            
         });
     }
 }
