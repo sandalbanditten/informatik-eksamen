9c9163a802fdd8b1b17fbf0d7c55299b5a3b2e64
sandalbanditten <sandalbanditten@tutanota.com>
2023-05-09 11:19:26 +0200

    Display the workouts in order of when they were made

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    // missing_docs // TODO: Turn this on at some moment
+)]
+// hide console window on Windows in release
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::collections::BTreeSet;
+
+use crate::workout::Workout;
 use eframe::egui;
+
+mod workout;
 
 fn main() -> Result<(), eframe::Error> {
       
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        initial_window_size: Some(egui::vec2(360.0, 800.0)),
         ..Default::default()
     };
-    eframe::run_native("rST", options, Box::new(|_cc| Box::<App>::default()))
+    let app = App::default();
+    eframe::run_native("rST", options, Box::new(|_| Box::new(app)))
 }
 
+/// The general state of our application
+#[derive(Debug, Clone)]
 struct App {
-    name: String,
-    age: u32,
+    /// A test
+    ///
+    /// Note: If two workouts are made at the same millisecond timestamp
+    /// only one of them will be inserted.
+    /// This is a highly improbably edge case.
+    workouts: BTreeSet<Workout>,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            name: "Arthur".to_owned(),
-            age: 42,
+            workouts: BTreeSet::new(),
         }
     }
 }
@@ -30,16 +47,20 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("rST");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
+            ui.separator();
+
+            // Workouts
+            ui.heading("Workouts:");
+            for (i, workout) in self.workouts.iter().enumerate() {
+                ui.label(format!("Workout {i}:"));
+                ui.label(format!("  Reps: {}", workout.reps()));
+                ui.label(format!(
+                    "  Weight: {}",
+                    workout
+                        .weight()
+                        .map_or("No weight".into(), |w| format!("{w}"))
+                ));
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
     }
 }
